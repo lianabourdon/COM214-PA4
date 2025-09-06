b881446d85ab557fa8b21fa2b9431ef2ccd83bb7
Liana Bourdon <liana.bourdon@conncoll.edu>
2025-09-06 21:24:09 +0000

    Add Creative Coding page (Weeks 1–2) with PDF link and formatting

--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,9 @@ Rails.application.routes.draw do
   get "/design",     to: "profile#design"
   get "/photography",to: "profile#photography"
   get "/music",      to: "profile#music"
+  # AT101 (password-protected)
+  get "/at101",              to: "creative_code#index", as: :at101
+  get "/at101/weeks/:id",    to: "creative_code#week",  as: :at101_week
+
 end
 
