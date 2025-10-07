dad6c1bb17b64c8075588f9669e1cd3cb3e4a18c
Liana Bourdon <liana.bourdon@conncoll.edu>
2025-10-07 03:39:38 +0000

    Include sketch scripts in asset pipeline for Heroku

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -5,4 +5,12 @@
 //= link bootstrap/bootstrap.min.css
 //= link bootstrap/bootstrap.bundle.min.js
 //= link_directory ../videos .mp4
+//= link_directory ../javascripts .js
+//= link sketch1.js
+//= link sketch2.js
+//= link sketch3.js
+//= link sketch4.js
+//= link sketch5.js
+//= link sketch6.js
+//= link sketch7.js
 
